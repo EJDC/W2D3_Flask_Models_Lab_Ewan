8ec95d75537a8bc6e61f32c6cf09e14b5f7ab936
user <user@users-MacBook-Pro.local>
2022-11-03 09:32:30 +0000

    Advanced Extensions complete

--- models/order_list.py
+++ models/order_list.py
@@ -1,6 +1,7 @@
 from models.order import *
 
 order1 = Order("Effie", "1/11/2022", 5, "The Lord of the Rings - The Fellowship of The Ring", "JRR Tolkien", "Fantasy", "The Hobbits, Gandalf, and the Fellowship of the Ring set off for Mordor")
-order2 = Order("Ewan", "2/11/2022", 8, "The Two Towers", "JRR Tolkien", "Fantasy", "Saruman betrays the light, battles at Helm's Deep and Isengard")
+order2 = Order("Ewan", "2/11/2022", 8, "The Lord of the Rings - The Two Towers", "JRR Tolkien", "Fantasy", "Saruman betrays the light, battles at Helm's Deep and Isengard")
+order3 = Order("Bob", "31/09/2022", 2, "The Lord of the Rings - Return of the King", "JRR Tolkien", "Fantasy", "Saruman betrays the light, battles at Helm's Deep and Isengard")
 
-orders = [order1, order2]
+orders = [order1, order2, order3]
